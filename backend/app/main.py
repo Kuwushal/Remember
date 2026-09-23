c266060bae43fc60362d3e02b41125b95d2b513e
Kuwushal <kuwushal1928@gmail.com>
2026-09-23 18:47:56 +0545

    add README and env example

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from app.routers import items
+from app.routers import items, ai
+
 
 app = FastAPI(title="Finder")
 
 app.include_router(items.router)
+app.include_router(ai.router)
 
 @app.get("/")
 def root():
